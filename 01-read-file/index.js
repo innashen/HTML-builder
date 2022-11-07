3933088d3f103a61422828ca92e2edddf217bb65
innashen <101499971+innashen@users.noreply.github.com>
2022-11-07 15:56:51 +0300

    feat: add solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFile = path.join(__dirname, 'text.txt');
+const readFile = fs.createReadStream(pathFile);
+
+readFile.on('data', (data) => {
+    console.log(data.toString());
+});
